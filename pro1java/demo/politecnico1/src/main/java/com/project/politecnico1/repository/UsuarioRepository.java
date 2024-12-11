b7870ffb6013d574908ab0af2d4d2c6b3ee3fc1f
YulianV-dev <juliov10suarez@gmail.com>
2024-12-11 12:21:22 -0500

    Delete pro1java/demo/politecnico1 directory

--- pro1java/demo/politecnico1/src/main/java/com/project/politecnico1/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.politecnico1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.politecnico1.models.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
-
-}
